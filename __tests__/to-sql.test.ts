4cbedd18a1949555fa9a032a6ec54442a42e9fdd
Edson Junior <edsonadeo@gmail.com>
2022-03-15 15:51:16 -0300

    Update to-sql.test.ts

--- __tests__/to-sql.test.ts
+++ __tests__/to-sql.test.ts
@@ -12,4 +12,16 @@ describe('toSQL - Function', () => {
     expect(sql).toEqual(expectedSQL)
     expect(arrParams).toEqual(expectedArr)
   })
+  
+  it('should not add param to query when it was not found', () => {
+    const expectedSQL = "SELECT * FROM table WHERE name = $1"
+    const expectedArr = ['xstarman']
+    const query = "SELECT * FROM table WHERE name = :name"
+    const params = { name: 'xstarman', teste: 'not-found' }
+
+    const [sql, arrParams] = toSQL<{ name: string }>(query, params)
+
+    expect(sql).toEqual(expectedSQL)
+    expect(arrParams).toEqual(expectedArr)
+  })
 })
